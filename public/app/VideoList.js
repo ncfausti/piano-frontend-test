ac35d18b1e9868ee0858d3825dbcfa405991990a
Warren Kushner <warren.kushner@gmail.com>
2017-05-14 18:14:46 +0000

    added create modal

--- public/app/VideoList.js
+++ public/app/VideoList.js
@@ -25,39 +25,73 @@ export default class VideoList extends React.Component {
 					
 				}
 				</ul>
+				<div> + <VideoModal /></div>
 			</div>
 		);
 	}
 }
  
 class VideoModal extends React.Component {
-
+    
 	render(){
-		
-		return (
-				<span>
-					<a  className="" style={{cursor:"pointer"}} data-toggle="modal" data-target={`#myModal`+this.props.i}>{this.props.title}</a>
-					
-					
-					<div id={"myModal"+this.props.i} className="modal fade" role="dialog">
-					  <div className="modal-dialog">
-					    <div className="modal-content">
-					      <div className="modal-header">
-					        <button type="button" className="close" data-dismiss="modal">&times;</button>
-					        <h4 className="modal-title">{this.props.title}</h4>
-					      </div>
-					      <div className="modal-body">
-					        <p>Some text in the modal.</p>
-					      </div>
-					      <div className="modal-footer">
-					        <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-					      </div>
-					    </div>
-					
-					  </div>
-					</div>
-			</span>
-        );
+		if(this.props.i>=0){
+			return (
+					<span>
+						<a  className="" style={{cursor:"pointer"}} data-toggle="modal" data-target={`#myModal`+this.props.i}>{this.props.title}</a>
+						
+						
+						<div id={"myModal"+this.props.i} className="modal fade" role="dialog">
+						  <div className="modal-dialog">
+						    <div className="modal-content">
+						      <div className="modal-header">
+						        <button type="button" className="close" data-dismiss="modal">&times;</button>
+						        <h4 className="modal-title">{this.props.title}</h4>
+						      </div>
+						      <div className="modal-body">
+						        <div>title: {this.props.title}</div>
+						        <div>published Date: {this.props.publishedDate}</div>
+						        <div>cover Image: {this.props.coverImage}</div>
+						        <div>sort Order: {this.props.sortOrder}</div>
+						      </div>
+						      <div className="modal-footer">
+						        <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+						      </div>
+						    </div>
+						
+						  </div>
+						</div>
+				</span>
+		);
+		}else{
+				return(
+					<span>
+						<a  className="" style={{cursor:"pointer"}} data-toggle="modal" data-target={`#myModal`}>add a video</a>
+						
+						
+						<div id={"myModal"} className="modal fade" role="dialog">
+						  <div className="modal-dialog">
+						    <div className="modal-content">
+						      <div className="modal-header">
+						        <button type="button" className="close" data-dismiss="modal">&times;</button>
+						        <h4 className="modal-title">add a video: </h4>
+						      </div>
+						      <div className="modal-body">
+						        <div>title: </div>
+						        <div>published Date: </div>
+						        <div>cover Image: </div>
+						        <div>sort Order: </div>
+						      </div>
+						      <div className="modal-footer">
+						        <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+						      </div>
+						    </div>
+						
+						  </div>
+						</div>
+				</span>
+				)
+		}
+        
 	}
 }
  
